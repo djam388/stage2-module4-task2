21f3a972914a6fb8e48716c97fb465190cbaad34
dadabaej <djam388@hotmail.com>
2023-01-15 01:00:31 +0500

    Attempt #9

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ConnectionPool.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ConnectionPool.java
@@ -40,8 +40,14 @@ public class ConnectionPool {
 
     public void releaseConnection(Connection connection) {
         if (connection.getClass() == ProxyConnection.class) {
-            usedConnections.remove();
-            freeConnections.offer((ProxyConnection) connection);
+            if (this.getFreeConnectionsCount() < 1) {
+//                throw new RuntimeException("No connection to close!");
+                return;
+            }
+                usedConnections.remove();
+                freeConnections.offer((ProxyConnection) connection);
+
+
         } else {
             throw new RuntimeException(String.format("Wrong connection is detected: %s, should be ProxyConnection.class ", connection.getClass()));
         }
